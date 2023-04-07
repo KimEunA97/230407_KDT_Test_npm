e6f30ea2a6c6f6ea31763ec0c356db0aa4bc9ebd
KimEunA97 <kimeuna9712@gmail.com>
2023-04-07 12:48:56 +0900

    npm i date

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-sad
+import http from 'http'
+import fs from 'fs'
+
+
+// const server = http.createServer(function (req, res) {
+
+
+//   fs.readFileSync('./index.html')
+
+
+//   res.writeHead(200, { 'Content-Type': 'text/html' });
+//   res.end();
+
+
+// })
+
+// server.listen(3050, function (error) {
+
+
+//   if (error) {
+
+//     console.log("서버 에러");
+
+//   }
+
+//   else {
+
+//     console.log("서버 구동 중...");
+
+//   }
+
+
+// })
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(new Date().toLocaleTimeString());
+});
+
+app.listen(3050, () => {
+  console.log('Server started on port 3050');
+});
